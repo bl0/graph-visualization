8c44210a4aad01beccd615afeaf5acf206a7b3db
bin liu <bin liu>
2015-12-26 17:19:24 +0800

    做了第一个任务“

--- js/functions.js
+++ js/functions.js
@@ -43,11 +43,16 @@ function node_init()
     
 }
 
+function link_width(weight)
+{
+  return Math.sqrt(weight)/2;
+}
+
 function link_init()
 {
   link.style("stroke", "#999")
     .style("stroke-opacity", 0.6)
-    .style("stroke-width", function(d){return Math.sqrt(d.weight)/2;});
+    .style("stroke-width", function(d){return link_width(d.weight)});
 }
 
 function init()
@@ -56,10 +61,55 @@ function init()
   node_init();
 }
 
+function get_path(i, j, path, index)
+{
+  var targetNode  = document.getElementById(i.toString()+"_node");
+  path[index++] = parseInt(targetNode.id);
+  if(i == j)
+   ;
+  else if(paths[i][j] == Number.POSITIVE_INFINITY) 
+  {
+    var targetNode  = document.getElementById(j.toString()+"_node");
+    path[index++] = parseInt(targetNode.id);
+  }
+  else
+  {
+    index = get_path(i, paths[i][j], path, index);
+    index = get_path(paths[i][j], j, path, index);
+  } 
+  return index;
+}
+
 function path_display()
 {
   init();
-  alert("path_display");
+  var sourceIndex = parseInt(document.getElementsByClassName("source")[0].value);
+  var targetIndex = parseInt(document.getElementsByClassName("target")[0].value);
+  var sourceNode  = document.getElementById(sourceIndex.toString()+"_node");
+  var targetNode  = document.getElementById(targetIndex.toString()+"_node");
+  var path = new Array(50);
+  var index = 0;
+  index = get_path(sourceIndex, targetIndex, path, 0);
+  node.style("r", function(d){
+    for(var i = 0; i < index; i ++)
+    {
+      if(path[i] == d.index)
+      {
+        return 5;
+      }
+    }
+    return 0;
+  });
+  link.style("stroke-width", function(d){
+    for(var i = 0; i < index-1; i ++)
+    {
+      if(path[i] == d.source.index && path[i+1] == d.target.index)
+      {
+        return link_width(d.weight);
+      }
+    }
+    return 0;
+  });
 }
 
 function mst_display()
